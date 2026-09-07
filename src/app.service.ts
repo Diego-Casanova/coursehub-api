6e825f7b67f987b1d0a7013346a6cbb91e24b9db
Unlimon135 <diegosebastiancc05@gmail.com>
2026-09-07 14:17:01 -0500

    Hello World cambiado a otro mensaje en el app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'coursehub-api está en linea';
   }
 }
